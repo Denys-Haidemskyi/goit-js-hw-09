2a10928d696f182bc9c258838d5dbaa44f9eca87
Den0v0 <122185695+Denys-Haidemskyi@users.noreply.github.com>
2024-02-10 19:28:07 +0100

    last update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,15 +1,11 @@
-// 2-form.js
 document.addEventListener('DOMContentLoaded', function () {
   const feedbackForm = document.querySelector('.feedback-form');
 
-  // Подія input
   feedbackForm.addEventListener('input', function (event) {
     if (event.target.name === 'email' || event.target.name === 'message') {
-      // Зчитування поточних значень полів форми
       const email = feedbackForm.elements['email'].value.trim();
       const message = feedbackForm.elements['message'].value.trim();
 
-      // Збереження у локальне сховище
       localStorage.setItem(
         'feedback-form-state',
         JSON.stringify({ email, message })
@@ -17,7 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  // Перевірка та відновлення стану під час завантаження сторінки
   const storedState = localStorage.getItem('feedback-form-state');
   if (storedState) {
     const { email, message } = JSON.parse(storedState);
@@ -25,19 +20,17 @@ document.addEventListener('DOMContentLoaded', function () {
     feedbackForm.elements['message'].value = message;
   }
 
-  // Подія submit
   feedbackForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // Перевірка обов'язкових полів перед відправленням
     const email = feedbackForm.elements['email'].value.trim();
     const message = feedbackForm.elements['message'].value.trim();
 
     if (email && message) {
-      // Вивід у консоль та очищення сховища та полів форми
       console.log({ email, message });
       localStorage.removeItem('feedback-form-state');
       feedbackForm.reset();
     }
   });
 });
+// DOne //
